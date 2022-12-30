a601b5b0ee4e18fcbaf5696894309de08921319e
Yuvraj <yuvraj.tomar@admitkard.com>
2022-12-31 00:00:09 +0530

    added backend configurations

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./configs/connectDB.js";
+import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.json());
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+//for creating production ready react app
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running !!!!!!");
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);

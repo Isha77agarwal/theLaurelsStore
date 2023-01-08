ea41a6ae856fbd07b76c5da005193625cd7eb8cc
Isha Agarwal <ishaagarwal772000@gmail.com>
2023-01-08 22:11:49 +0530

    Desktop header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,53 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './App.css'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeScreen />,
+  }, {
+    path: "product/:product",
+    element: <ProductScreen />
+  }
+])
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#333652',
+      light: '#5e607e',
+      dark: '#0c1029',
+      contrastText: '#e9eaec'
+    },
+    secondary: {
+      main: '#fad02c',
+      light: '#ffff64',
+      dark: '#F0C82B',
+      contrastText: '#000000'
+    },
+    neutral: {
+      main: '#E9EAEC',
+      contrastText: '#333652'
+    }
+  },
+  typography: {
+    fontFamily: [
+      'sans-serif'
+    ]
+  }
+})
+
 function App() {
+
   return (
-    <div className="text-3xl font-bold underline">
-      hello
-    </div>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/data/data.js
@@ -0,0 +1,10 @@
+export const categories = ['Gifting', 'Watches', 'Accessories', 'Bags', 'Fragrance']
+
+
+export const subcategories = {
+    'Gifting': ['Personal Care', 'Bluetooth Speaker', 'Gift Set', 'Headphones', 'Perfumes', 'Table Lamp', 'Saregama Carvan', 'Home Theatre'],
+    'Watches': ['Analogue Watch', 'Smart Watch', 'Smart Band'],
+    'Accessories': ['Artificial Jwellery', 'Bracelet', 'Clutch', 'Handbag', 'Earbuds', 'Neckband', 'Wired Earphones', 'Belt', 'Wallet', 'Sunglass', 'Mobile Accessories'],
+    'Bags': ['Messenger Bag', 'Sling Bag', 'Duffle Bag', 'Laptop Bag', 'Bagpack'],
+    'Fragrance': ['Deo', 'Perfume']
+}
